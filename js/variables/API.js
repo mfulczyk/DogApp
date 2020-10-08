67609da3e80daf00a775073c45e60553f6be60db
mfulczyk <fulczyk.michal@gmail.com>
2020-10-08 08:54:57 +0200

    test update

--- js/variables/API.js
+++ js/variables/API.js
@@ -2,6 +2,9 @@ const API_URL_BREEDS="http://localhost:3000/dogs/"
 const API_URL_USERDOGS="http://localhost:3000/userDogs/"
 
 const SERVER="192.168.0.143:3000"
+// const SERVER="localhost:3000"
+
+
 
 
 
